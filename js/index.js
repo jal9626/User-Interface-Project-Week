5a41d8fad4dd681dc020d82cbc821c2ec010ff95
Ashley <jal9626@gmail.com>
2019-02-20 20:50:34 -0500

    dropdown completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // JS goes here
+class Dropdown {
+    constructor(element) {
+        //assign this element to the dropdown element
+        this.element = element;
+        // console.log(this.element);
+        // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
+        this.button = this.element.querySelector('.hamburger');
+        // console.log(this.button);
+        // assign the reference to the ".dropdown-content" class found in the dropdown element
+        this.content = this.element.querySelector('.menu-content');
+        console.log(this.content);
+        this.hamburger = this.element.querySelector('.hamburger-close');
+        console.log(this.hamburger);
+        this.button.addEventListener('click', () => {
+            this.toggleContent();
+        })
+    }
+
+    toggleContent() {
+        this.content.classList.toggle('menu-open');
+    }
+
+    
+}
+
+let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
